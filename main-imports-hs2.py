8297fe3f2ffe3560c04cc82ee8cebd38f42ca720
mikew <waugh.e.michael@gmail.com>
2022-07-05 15:57:25 -0400

    import data for april

--- main-imports-hs2.py
+++ main-imports-hs2.py
@@ -23,7 +23,7 @@ from bokeh.transform import factor_cmap
 # Alot of this was built of this "cross-fire demo"
 # https://github.com/bokeh/bokeh/blob/branch-2.3/examples/app/crossfilter/main.py
 
-final_month = 7
+final_month = 8
 final_year = 2022
 
 background = "#ffffff"
