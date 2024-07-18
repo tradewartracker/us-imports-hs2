e54619886dcdcc66c6100fedb89cfd37e34770be
mikew <waugh.e.michael@gmail.com>
2024-07-18 11:16:23 -0400

    May data update

--- main-imports-hs2.py
+++ main-imports-hs2.py
@@ -23,7 +23,7 @@ from bokeh.transform import factor_cmap
 # Alot of this was built of this "cross-fire demo"
 # https://github.com/bokeh/bokeh/blob/branch-2.3/examples/app/crossfilter/main.py
 
-final_month = 9
+final_month = 10
 final_year = 2024
 
 background = "#ffffff"
