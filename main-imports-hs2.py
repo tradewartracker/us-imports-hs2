d4891fe3af15d95fd31bc004e799aae7b9959048
mikew <waugh.e.michael@gmail.com>
2026-02-02 20:04:57 -0500

    major update

--- main-imports-hs2.py
+++ main-imports-hs2.py
@@ -11,12 +11,14 @@ import pyarrow.parquet as pq
 from bokeh.io import curdoc
 from bokeh.layouts import column, gridplot, row
 from bokeh.models import ColumnDataSource, DataRange1d, Select, HoverTool, Panel, Tabs, LinearColorMapper, Range1d, MultiChoice
-from bokeh.models import NumeralTickFormatter, Title, Label, Paragraph, Div, CustomJSHover, BoxAnnotation
+from bokeh.models import NumeralTickFormatter, Title, Label, Paragraph, Div, CustomJSHover, BoxAnnotation, Button
 from bokeh.models import ColorBar
 from bokeh.palettes import brewer, Spectral6
 from bokeh.plotting import figure
 from bokeh.embed import server_document
 from bokeh.transform import factor_cmap
+import io
+import base64
 
 #################################################################################
 # This just loads in the data...
@@ -64,38 +66,54 @@ def make_plot():
     height = int(1.15*533)
     width = int(1.15*750)
     
-    foo = df.loc[(country_select.value, product_select.value),:]
-    # below there is an object of selections which will be one of the values in 
-    # the list of options. So the .value then grabs that particular option selected
+    # Debug: Print what we're trying to select
+    #print(f"\n=== DEBUG INFO ===")
+    #print(f"Selected countries: {country_select.value}")
+    #print(f"Selected product: {product_select.value}")
+    #print(f"Selected level: {level_select.value}")
+    #print(f"DataFrame index levels: {df.index.names}")
+    #print(f"DataFrame columns: {df.columns.tolist()}")
     
-    if level_select.value == 'US Dollars':
-        level_series = "imports"
-        
-        lead_title = "US Imports from " 
-        
-    if level_select.value == 'Tariff Revenue':
-        level_series = "duty"
-        
-        lead_title  = "US Tariff Revenue on " 
-        
-    if level_select.value == 'Implied Tariff':
-        level_series = "itariff"
-        
-        lead_title  = "US Implied Tariff on " 
+    # Select data for the chosen product and all selected countries
+    # Need to use pd.IndexSlice for proper MultiIndex slicing
+    idx = pd.IndexSlice
+    foo = df.loc[idx[country_select.value, product_select.value, :], :]
+    
+    #print(f"After slicing, foo shape: {foo.shape}")
+    #print(f"After slicing, foo index: {foo.index.names}")
+    #if len(foo) > 0:
+    #    print(f"First few rows of foo:\n{foo.head()}")
+    #else:
+    #    print("WARNING: foo is empty!")
+    
+    # below there is an object of selections which will be one of the values in 
+    # the list of options. So the .value then grabs that particular option selected.
         
     if level_select.value == 'Year over Year % Change':
-        
-        foo = foo.groupby(["CTY_NAME"]).apply(growth_trade).reset_index(level=0, drop=True)
-        # the .reset_index seems to be needed for the groupby to work properly on pandas > 3.0
+
+        foo = foo.groupby(level=0).apply(growth_trade)
 
         level_series = "growth"
         
-        lead_title = "US Imports from " 
-
-        # Debugging: Print the content of DataFrame 'foo' after grouping
-        #print("DataFrame 'foo' after grouping:")
-        #print(foo.head())
-        #print("foo index levels:", foo.index.names)
+        lead_title = "US Imports from "
+        
+    if level_select.value == 'US Dollars':
+        
+        level_series = "imports"
+        
+        lead_title = "US Imports from "
+        
+    if level_select.value == 'Tariff Revenue':
+        
+        level_series = "duty"
+        
+        lead_title = "US Tariff Revenue on "
+        
+    if level_select.value == 'Implied Tariff':
+        
+        level_series = "itariff"
+        
+        lead_title = "US Implied Tariff on "
         
     title_name = ""
         
@@ -107,81 +125,122 @@ def make_plot():
         if len(country_select.value) > 2:
             title_name = title_name + name[0:3] + ", "
         
-    title = lead_title + title_name + "of " + product_select.value.title().upper()    
+        
+    title = lead_title + title_name + "of " + product_select.value.title().upper()
 
+    # This is standard bokeh stuff so far
     plot = figure(x_axis_type="datetime", plot_height = height, plot_width=width, toolbar_location = 'below',
-           tools = "box_zoom, reset, pan, xwheel_zoom", title = title,
+           tools = "box_zoom, reset, pan, xwheel_zoom, save", title = title,
                   x_range = (dt.datetime(2017,7,1),dt.datetime(final_year,final_month,1)) )
-
-    numlines=len(country_select.value)
     
-    multi_line_source = ColumnDataSource({
-            'xs': [foo.index.get_level_values(2).unique()]*numlines,
-            'ys': [foo.loc[(name, product_select.value),level_series].values for name in country_select.value],
-            'label': [name for name in country_select.value]})
-
-    plot.multi_line(xs= "xs",
-                ys= "ys",
-                line_width=5, line_alpha=0.75, line_color = "slategray",
-                hover_line_alpha=0.75, hover_line_width = 5,
-                hover_line_color= "crimson", source = multi_line_source)
+    # Get fixed colors from the dataframe for each selected country
+    line_colors = []
+    line_widths = []
+    
+    for country_name in country_select.value:
+        # Get the color for this country from the dataframe
+        country_data = foo.loc[country_name]
+        country_color = country_data.iloc[0]['color']
+        line_colors.append(country_color)
+        line_widths.append(5)
+    
+    # Plot each country as a separate line for legend support
+    for i, country_name in enumerate(country_select.value):
+        country_data = foo.loc[country_name]
+        
+        # Since we sliced by country, we still have ['I_COMMODITY_SDESC', 'time'] as index
+        # We need to get the time values from the index
+        time_values = country_data.index.get_level_values('time')
+        
+        # Create ColumnDataSource with flag URL
+        source = ColumnDataSource(data=dict(
+            x=time_values,
+            y=country_data[level_series].values,
+            country=[country_name] * len(country_data),
+            flag=[country_data['flag'].iloc[0]] * len(country_data)
+        ))
+        
+        plot.line('x', 'y', source=source,
+                 line_width=line_widths[i], line_alpha=0.75, line_color=line_colors[i],
+                 legend_label=country_name, name=country_name)
         
     # fixed attributes
     plot.xaxis.axis_label = None
     plot.yaxis.axis_label = ""
     plot.axis.axis_label_text_font_style = "bold"
     plot.grid.grid_line_alpha = 0.3
-    
-    TIMETOOLTIPS = """
-            <div style="background-color:#F5F5F5; opacity: 0.95; border: 5px 5px 5px 5px;">
-            <div style = "text-align:left;">
-            <span style="font-size: 13px; font-weight: bold"> @label
-             </span>
-             </div>
-             <div style = "text-align:left;">"""
-    
+
+    # Configure tooltips with flag images
     if level_select.value == 'Year over Year % Change':
-    
-        TIMETOOLTIPS = TIMETOOLTIPS + """
-            <span style="font-size: 13px; font-weight: bold"> $data_x{%b %Y}:  $data_y{0.0}%</span>   
-            </div>
-            </div>
-            """
-        
-        plot.add_tools(HoverTool(tooltips = TIMETOOLTIPS,  line_policy='nearest', formatters={'$data_x': 'datetime'}))
+        TIMETOOLTIPS = """
+                <div style="background-color:#F5F5F5; opacity: 0.95; border: 5px 5px 5px 5px;">
+                <div style = "text-align:left;">
+                <span style="font-size: 13px; font-weight: bold">
+                <img src="@flag" alt="" style="height:20px; vertical-align:middle; margin-right:8px;"> @country
+                 </span>
+                 </div>
+                 <div style = "text-align:left;">
+                <span style="font-size: 13px; font-weight: bold"> @x{%b %Y}:  @y{0}%</span>   
+                </div>
+                </div>
+                """
+        plot.add_tools(HoverTool(tooltips = TIMETOOLTIPS,  line_policy='nearest', formatters={'@x': 'datetime'}))
         
     if level_select.value == 'US Dollars':
-    
-        TIMETOOLTIPS = TIMETOOLTIPS + """
-            <span style="font-size: 13px; font-weight: bold"> $data_x{%b %Y}:  $data_y{$0.0a}</span>   
-            </div>
-            </div>
-            """
-        plot.add_tools(HoverTool(tooltips = TIMETOOLTIPS,  line_policy='nearest', formatters={'$data_x': 'datetime'}))
+        TIMETOOLTIPS = """
+                <div style="background-color:#F5F5F5; opacity: 0.95; border: 5px 5px 5px 5px;">
+                <div style = "text-align:left;">
+                <span style="font-size: 13px; font-weight: bold">
+                <img src="@flag" alt="" style="height:20px; vertical-align:middle; margin-right:8px;"> @country
+                 </span>
+                 </div>
+                 <div style = "text-align:left;">
+                <span style="font-size: 13px; font-weight: bold"> @x{%b %Y}:  @y{$0.0a}</span>   
+                </div>
+                </div>
+                """
+        plot.add_tools(HoverTool(tooltips = TIMETOOLTIPS,  line_policy='nearest', formatters={'@x': 'datetime'}))
         
     if level_select.value == 'Tariff Revenue':
-    
-        TIMETOOLTIPS = TIMETOOLTIPS + """
-            <span style="font-size: 13px; font-weight: bold"> $data_x{%b %Y}:  $data_y{$0.0a}</span>   
-            </div>
-            </div>
-            """
-        plot.add_tools(HoverTool(tooltips = TIMETOOLTIPS,  line_policy='nearest', formatters={'$data_x': 'datetime'}))
+        TIMETOOLTIPS = """
+                <div style="background-color:#F5F5F5; opacity: 0.95; border: 5px 5px 5px 5px;">
+                <div style = "text-align:left;">
+                <span style="font-size: 13px; font-weight: bold">
+                <img src="@flag" alt="" style="height:20px; vertical-align:middle; margin-right:8px;"> @country
+                 </span>
+                 </div>
+                 <div style = "text-align:left;">
+                <span style="font-size: 13px; font-weight: bold"> @x{%b %Y}:  @y{$0.0a}</span>   
+                </div>
+                </div>
+                """
+        plot.add_tools(HoverTool(tooltips = TIMETOOLTIPS,  line_policy='nearest', formatters={'@x': 'datetime'}))
         
     if level_select.value == 'Implied Tariff':
-    
-        TIMETOOLTIPS = TIMETOOLTIPS + """
-            <span style="font-size: 13px; font-weight: bold"> $data_x{%b %Y}:  $data_y{0.0}</span>   
-            </div>
-            </div>
-            """
-        plot.add_tools(HoverTool(tooltips = TIMETOOLTIPS,  line_policy='nearest', formatters={'$data_x': 'datetime'}))
+        TIMETOOLTIPS = """
+                <div style="background-color:#F5F5F5; opacity: 0.95; border: 5px 5px 5px 5px;">
+                <div style = "text-align:left;">
+                <span style="font-size: 13px; font-weight: bold">
+                <img src="@flag" alt="" style="height:20px; vertical-align:middle; margin-right:8px;"> @country
+                 </span>
+                 </div>
+                 <div style = "text-align:left;">
+                <span style="font-size: 13px; font-weight: bold"> @x{%b %Y}:  @y{0.0}</span>   
+                </div>
+                </div>
+                """
+        plot.add_tools(HoverTool(tooltips = TIMETOOLTIPS,  line_policy='nearest', formatters={'@x': 'datetime'}))
                 
     if level_select.value == 'Year over Year % Change':
         if foo[level_series].max() > 1500:
             plot.y_range.end = 1500
     
-    
+    # Configure legend
+    plot.legend.location = "top_left"
+    plot.legend.click_policy = "hide"  # Click to hide/show lines
+    plot.legend.label_text_font_size = "10pt"
+    plot.legend.spacing = 2
+    plot.legend.padding = 5
     
     plot.title.text_font_size = '13pt'
     plot.background_fill_color = background 
@@ -232,6 +291,62 @@ def make_plot():
 
 #################################################################################
 
+def download_csv():
+    """Generate CSV data for currently selected countries, product, and metric"""
+    
+    # Get the data for selected countries and product
+    idx = pd.IndexSlice
+    foo = df.loc[idx[country_select.value, product_select.value, :], :]
+    
+    if level_select.value == 'Year over Year % Change':
+        foo = foo.groupby(level=0).apply(growth_trade)
+        metric_column = "growth"
+    elif level_select.value == 'US Dollars':
+        metric_column = "imports"
+    elif level_select.value == 'Tariff Revenue':
+        metric_column = "duty"
+    elif level_select.value == 'Implied Tariff':
+        metric_column = "itariff"
+    else:
+        metric_column = "imports"
+    
+    # Collect data for all selected countries
+    data_list = []
+    for country_name in country_select.value:
+        country_data = foo.loc[country_name]
+        for idx_val, row in country_data.iterrows():
+            value = row[metric_column]
+            # Skip rows where the metric value is NaN or missing
+            if pd.notna(value):
+                # idx_val is a tuple (product, time) - extract the time component
+                time_val = idx_val[1] if isinstance(idx_val, tuple) else idx_val
+                data_list.append({
+                    'Country': country_name,
+                    'Product': product_select.value,
+                    'Date': time_val.strftime('%Y-%m-%d'),
+                    'Metric': level_select.value,
+                    'Value': value
+                })
+    
+    # Create DataFrame and export to CSV
+    export_df = pd.DataFrame(data_list)
+    csv_string = export_df.to_csv(index=False)
+    
+    # Create data URI for download link
+    b64 = base64.b64encode(csv_string.encode()).decode()
+    data_uri = f"data:text/csv;base64,{b64}"
+    
+    # Update the download link div
+    download_link_div.text = f'''
+    <a href="{data_uri}" download="import_data.csv" 
+       style="display:inline-block; padding:10px 20px; background-color:#28a745; 
+              color:white; text-decoration:none; border-radius:4px; font-weight:bold;">
+       Click Here to Download CSV
+    </a>
+    '''
+
+#################################################################################
+
 def update_plot(attrname, old, new):
     layout.children[0] = make_plot()
     
@@ -258,16 +373,27 @@ product_select = Select(value=product, title='HS2 Product', options=sorted(produ
 product_select.on_change('value', update_plot)
 # Change the value upone selection via the update plot 
 
+#################################################################################
+
+# Download CSV button and link
+download_button = Button(label="Generate CSV Download Link", button_type="success", width=350)
+download_button.on_click(download_csv)
+
+download_link_div = Div(text="", width=350, height=50)
+
+#################################################################################
+
 div0 = Div(text = """Each category is a 2 digit HS Code. ALL PRODUCTS is the sum of imports across all product catagories.\n
     """, width=350, background = background, style={"justify-content": "space-between", "display": "flex"} )
 
-div1 = Div(text = """Top 20 Countries by import volume and TOTAL which aggregates across all countries in the world.\n
+div1 = Div(text = """Top 20 Countries by import volume and TOTAL which aggregates across all countries in the world. Select multiple countries.\n
     """, width=350, background = background, style={"justify-content": "space-between", "display": "flex"} )
 
-div2 = Div(text = """Transformations: Dollars, Year over Year Percent Change, Tariff (Duty) Revenue, Implied Tariff\n
+div2 = Div(text = """<b>Download Chart:</b> Use the save icon (💾) in the chart toolbar to download as PNG.<br>
+    <b>Download Data:</b> Click the button to generate a download link for CSV data.\n
     """, width=350, background = background, style={"justify-content": "space-between", "display": "flex"} )
 
-controls = column(country_select,div1, product_select, div0, level_select,div2)
+controls = column(country_select, div1, product_select, div0, level_select, download_button, download_link_div, div2)
 
 height = int(1.95*533)
 width = int(1.95*675)
